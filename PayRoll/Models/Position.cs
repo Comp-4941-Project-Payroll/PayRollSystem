6c3271fdf229d01d248232c4ef212714f863a8ba
Davin Deol <davindeol@gmail.com>
2018-04-04 18:18:03 -0700

    Updated Framework + Initializer
    All ICollections<> are instantiated and the initializer calls base.Seed() at the end

--- PayRoll/Models/Position.cs
+++ PayRoll/Models/Position.cs
@@ -17,6 +17,6 @@ namespace PayRoll.Models
 		public string PositionName { get; set; }
 
 		public decimal BaseSalary { get; set; } = 0;
-        public ICollection<Employee> Employees { get; set; }
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
