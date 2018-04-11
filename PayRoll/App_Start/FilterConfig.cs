472437df13d5c446601b544bd835229b8b7254d4
Davin Deol <davindeol@gmail.com>
2018-04-11 16:30:41 -0700

    Updated "Change Password" so that it Doesn't Show the Password

--- PayRoll/App_Start/FilterConfig.cs
+++ PayRoll/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace PayRoll
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new System.Web.Mvc.AuthorizeAttribute());
         }
     }
 }
